c6ff981b9f283788980f4963d5b9fe26c702ae73
vishu reddy <saduvishwanath95@gmail.com>
2024-05-14 13:30:37 +0530

    add procfile,upate port,gitignore

--- app.js
+++ app.js
@@ -62,7 +62,10 @@ app.get("/posts/:postId", async function(req, res){
           content: post.content
         });
 });
-
-app.listen(3000, function() {
+let port=process.env.PORT;
+if(port==null || port==""){
+  port=3000;
+}
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
